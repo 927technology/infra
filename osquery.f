e098181f8e83f8744d6b64f4482773b152029607
Chris Murray <83325346+927technology@users.noreply.github.com>
2024-10-07 19:58:50 -0400

    Update osquery.f

--- osquery.f
+++ osquery.f
@@ -1,12 +1,17 @@
-function osquery.list {
-  _filter=${false}
-  _filter_type=
-  _json="{}"
-  _path=
-  _sane=${false}
-  _type=
+lfunction osquery.list {
 
-  # read args
+  # variables
+  local _filter=${false}
+  local _filter_type=
+  local _json="{}"
+  local _exit_code=${exit_crit}
+  local _path=
+  local _sane=${false}
+  local _type=
+
+  # main
+
+  ## read args
   while [[ ${1} != "" ]]; do
     case ${1} in
       --path | --p )
@@ -23,19 +28,46 @@ function osquery.list {
     shift
   done
   
-  # select table
+  ## select table
   case ${_table} in
     file )
-      filter=${true}
-      type=file
+      _filter=${true}
+      _filter_type=path
+      _type=file
+
+      # check sanity
+
+      if [[                         \
+          ${_filter} == ${true}  && \
+          ! -z ${filter_type}    && \   
+      ]]                            \
+      then
+        _sane=${true}
+
+      else
+
+      fi
     ;;
     * )
 
     ;;
 esac
 
-if [[ ${filter == ${true}; then
-  _json=$(${cmd_osqueryi} --json "select * from ${type} where path=${_path}")
-else
-  _json=$(${cmd_osqueryi} --json "select * from ${type}")
+
+
+
+## filter enabled
+if [[ ${filter == ${true} && ${_sane} ]]; then
+  _json=$(${cmd_osqueryi} --json "select * from ${type} where ${_filter_type}=${_path}")
+  ${cmd_jq} ${_json} 2&>1 /dev/null   && _exit_code=${exit_ok} || _json="{}"
+
+## filter disabled
+elif if [[ ${filter == ${false} && ${_sane} ]]; then
+  _json=$(${cmd_osqueryi} --json "select * from ${type}") 
+
+${cmd_jq} ${_json} 2&>1 /dev/null   && _exit_code=${exit_ok} || _json="{}"
 fi
+
+
+${cmd_echo} ${_json}
+exit ${_exit_code}
