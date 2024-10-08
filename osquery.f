abf5b672675267170afc0ca718528176ca23ed61
Chris Murray <83325346+927technology@users.noreply.github.com>
2024-10-07 22:32:43 -0400

    Update osquery.f

--- osquery.f
+++ osquery.f
@@ -4,10 +4,11 @@ lfunction osquery.list {
   local _filter=${false}
   local _filter_type=
   local _json="{}"
+  local _json_output="{}"
   local _exit_code=${exit_crit}
   local _path=
   local _sane=${false}
-  local _type=
+  local _table=
 
   # main
 
@@ -23,6 +24,10 @@ lfunction osquery.list {
         shift
         _table=$(text.lcase ${1})
       ;; 
+      * | -h | --help )
+        ${cmd_echo} need help dialog
+        exit ${exit_crit}
+      ;;
     esac
 
     shift
@@ -58,16 +63,32 @@ esac
 
 ## filter enabled
 if [[ ${filter == ${true} && ${_sane} ]]; then
-  _json=$(${cmd_osqueryi} --json "select * from ${type} where ${_filter_type}=${_path}")
-  ${cmd_jq} ${_json} 2&>1 /dev/null   && _exit_code=${exit_ok} || _json="{}"
+  _json_output=$(${cmd_osqueryi} --json "select * from ${type} where ${_filter_type}=${_path}")
+  ${cmd_jq} ${_json_output} 2&>1 /dev/null   && _exit_code=${exit_ok} || _json_output="{}"
 
 ## filter disabled
 elif if [[ ${filter == ${false} && ${_sane} ]]; then
-  _json=$(${cmd_osqueryi} --json "select * from ${type}") 
+  _json_output=$(${cmd_osqueryi} --json "select * from ${type}") 
 
-${cmd_jq} ${_json} 2&>1 /dev/null   && _exit_code=${exit_ok} || _json="{}"
+${cmd_jq} ${_json_output} 2&>1 /dev/null   && _exit_code=${exit_ok} || _json_output="{}"
 fi
 
+
+
+
+## write status to json
+_json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.exit_code |+= '${_exit_code})
+
+_json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.args.filter |+= '${_filter})
+
+_json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.args.filter_type |+= '${_filter_type})
+
+_json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.args.path |+= '${_path})
+
+_json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.args.table |+= '${_table})
+
+## write output to json
+_json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.data |+= '"${_json_output}")
 
 ${cmd_echo} ${_json}
 exit ${_exit_code}
