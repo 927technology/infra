a7544891fa84b6baec790c93fc43ba4afd8626ea
Chris Murray <83325346+927technology@users.noreply.github.com>
2024-10-10 18:46:35 -0400

    Update fs.f

--- fs.f
+++ fs.f
@@ -31,31 +31,69 @@ function fs.ramdrv {
   esac
 
   ## validate 
-  [[ -z ${_gid} ]]   && $(( _error_count++ ))
-  [[ -z ${_uid} ]]   && $(( _error_count++ ))
-  [[ -z ${_mount} ]] && $(( _error_count++ ))
+  if [[ ! -z ${_gid} ]]; then
+    _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.validate.gid |+= '${true})
+  else
+    $(( _error_count++ ))
+    _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.validate.gid |+= '${false})
+  fi
+
+
+  if [[ -z ${_uid} ]]; then
+    _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.validate.uid |+= '${true})
+  else
+    $(( _error_count++ ))
+    _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.validate.uid |+= '${false})
+  fi
+
+
+  if [[ -z ${_mount} ]]; then
+    _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.validate.mount |+= '${true})
+  else
+    $(( _error_count++ ))
+    _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.validate.mount |+= '${false})
+  fi
 
   ## create mount point
   if [[ ! -d ${_mount} && ${_error_count} == 0 ]]; then 
     ${cmd_mkdir} ${mount} 2>&1 /dev/null 
     if [[ ${?} ]]: then
+      _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.ramfs.mkdir |+= '${true})
+    else
       $(( _error_count++ ))
+      _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.ramfs.mkdir |+= '${false})
+    fi
 
 
+    ## set permissions
+    ${cmd_chown} ${_uid}:${_gid} ${_mount} 2>&1 /dev/null
+    if [[ ${?} ]]: then
+      _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.ramfs.chown |+= '${true})
+    else
+      $(( _error_count++ ))
+      _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.ramfs.chown |+= '${false})
+    fi
 
 
-
-
-
-
-
-    ${cmd_chown} ${_uid}:${_gid} ${_mount} 2>&1 /dev/null || $(( _error_count++ ))  
-
-    ${cmd_mount} -t ramfs -o size=1g  ramfsmount ${_mount} 2>&1 /dev/null || $(( _error_count++ ))
+    ## mount
+    ${cmd_mount} -t ramfs -o size=1g  ramfsmount ${_mount} 2>&1 /dev/null
+if [[ ${?} ]]: then
+      _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.ramfs.mount |+= '${true})
+    else
+      $(( _error_count++ ))
+      _json=$( ${cmd_echo} ${_json}  | ${cmd_jq} '.status.ramfs.mount |+= '${false})
+    fi
 
   fi
 
+  ## validate json
+  $( ${cmd_echo} ${}  )
+
+  ## set exit code
   [[ ${_error_count} == 0 ]] && _exit_code=${exitok}
+
+  ## exit
+  ${cmd_echo} ${_json}
 
   
 }
