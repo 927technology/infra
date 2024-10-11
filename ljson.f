384d509ddbaaa37676b68b72bf5ebab4f2b1c5fd
Chris Murray <83325346+927technology@users.noreply.github.com>
2024-10-10 20:33:15 -0400

    Update ljson.f

--- ljson.f
+++ ljson.f
@@ -1,11 +1,14 @@
 function json.validate {
+  # dependancies
+  ## bools.v
+  ## cmd*.v
 
   # variables 
   local _json=${1}
 
   # main
   ${cmd_jq} ${_json} 2&>1 /dev/null
-  if [[ ${?} ]]; then
+  if [[ ${?} == ${exitok} ]]; then
     ${cmd_echo} ${true}
     exit ${exitok}
   else
